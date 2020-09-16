a5bb1c461ba0edd0b73f7d81517176895698fe42
peploleum <peploleum@gmail.com>
2020-09-16 16:25:22 +0200

    fixed custom kibana config

--- src/main/docker/elasticsearch/build.sh
+++ src/main/docker/elasticsearch/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
 docker build --no-cache -t opendistro-for-elasticsearch-genius:0.10.0 .
--- src/main/docker/kibana/build-opendistro.sh
+++ src/main/docker/kibana/build-opendistro.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-rm -rf *.zip
-cp ../../../../kibana-extra/ingestion/build/ingestion-0.0.0.zip .
-cp ../../../../kibana-extra/vega-timeline/build/vega-timeline-0.0.0.zip .
-cp ../../../../kibana-extra/flights-scroll/build/flights-scroll-0.0.0.zip .
-
 docker build -t kibana-opendistro-plugin-genius:0.10.0 --no-cache .
